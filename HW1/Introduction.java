e139a293864867a1a5a92270309c6cc859cb8f06
Memematrix <thelegendbrick678@gmail.com>
2025-09-18 21:38:11 +0700

    Add folder structure

---
+++ HW1/Introduction.java
@@ -0,0 +1,30 @@
+package HW1;
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.print("Dương Đăng Khoa\t");
+        System.out.print("24020181\t");
+        System.out.print("INT2204 1\t");
+        System.out.print("Memematrix\t");
+        System.out.println("24020181\t");
+        System.out.println("9 bottles of beer on the wall, 9 bottles of beer.");
+        System.out.println("Take one down, pass it around, ");
+        System.out.println("8 bottles of beer on the wall, 8 bottles of beer.");
+        System.out.println("Take one down, pass it around,");
+        System.out.println("7 bottles of beer on the wall, 7 bottles of beer.");
+        System.out.println("Take one down, pass it around, ");
+        System.out.println("6 bottles of beer on the wall, 6 bottles of beer.");
+        System.out.println("Take one down, pass it around,");
+        System.out.println("5 bottles of beer on the wall, 5 bottles of beer.");
+        System.out.println("Take one down, pass it around, ");
+        System.out.println("4 bottles of beer on the wall, 4 bottles of beer.");
+        System.out.println("Take one down, pass it around,");
+        System.out.println("3 bottles of beer on the wall, 3 bottles of beer.");
+        System.out.println("Take one down, pass it around, ");
+        System.out.println("2 bottles of beer on the wall, 2 bottles of beer.");
+        System.out.println("Take one down, pass it around,");
+        System.out.println("1 bottle of beer on the wall, 1 bottle of beer.");
+        System.out.println("Take one down, pass it around,");
+        System.out.println("No more bottles of beer on the wall.");
+    }
+
+}
